283a208e695ae0f0b5f981a515b93f05e13f431f
Jordan Collier <jordanicollier@gmail.com>
2015-02-25 14:26:06 -0700

    add relation between user, reviews and company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,4 @@
 class Company < ActiveRecord::Base
-
-
+  has_many :products
+  has_many :company_reviews
 end
--- app/models/product_review.rb
+++ app/models/product_review.rb
@@ -1,4 +1,5 @@
 class ProductReview < ActiveRecord::Base
-
+  belongs_to :product
+  belongs_to :user
 
 end
